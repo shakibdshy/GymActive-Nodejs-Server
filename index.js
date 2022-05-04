8ec83cc70d72a8be7d639d0fba97065d61c5b3ad
Shakib D shy <shakibdshy@gmail.com>
2022-05-04 22:29:16 +0600

    DB Connect & Inventory Item Update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -7,6 +8,25 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9i21l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const inventoryCollection = client.db('gymActive').collection('inventory');
+
+        // Get Service
+        app.get('/inventory', async(req, res) => {
+            const query = {};
+            const inventory = await inventoryCollection.find(query).toArray();
+            res.send(inventory);
+        });
+    }
+    finally {}
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Running Server');
