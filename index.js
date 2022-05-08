5551afeed8c86c8568040ecb0357026f73d78faf
Shakib D shy <shakibdshy@gmail.com>
2022-05-08 19:32:26 +0600

    auth post api update

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ async function run() {
 
         // Authentication
         app.post('/login', (req, res) => {
-            const { username } = req.body;
-            const accessToken = jwt.sign({ username }, process.env.ACCESS_TOKEN_SECRET, {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
                 expiresIn: '1d'
             });
             res.send({ accessToken });
